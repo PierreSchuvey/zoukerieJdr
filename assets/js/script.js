546762f15a0470dad1cc221c50ff7610329ef380
PierreSchuvey <pierre.schuveye2n@gmail.com>
2021-03-10 16:35:32 +0100

    pokemon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 $("video").css("display","none");
+$("video2").css("display","none");
 $("#rolling").on("click",function() {
+  if($("#nbDices").val() == "Pokémon"){
+        $("#video2").css("display","block");
+        $("#video2")[0].autoplay = true;
+  }
 var nbSix = 0;
 var nbOne = 0;
 $( ".dice" ).remove();
